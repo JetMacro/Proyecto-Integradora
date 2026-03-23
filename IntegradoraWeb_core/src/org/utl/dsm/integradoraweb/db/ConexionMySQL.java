ffd681bacd4e13d626c6866b08ae0dd820d17090
JetMacrogamer <marcosalbzer@gmail.com>
2026-03-23 01:27:33 -0600

    porfavor

--- IntegradoraWeb_core/src/org/utl/dsm/integradoraweb/db/ConexionMySQL.java
+++ IntegradoraWeb_core/src/org/utl/dsm/integradoraweb/db/ConexionMySQL.java
@@ -9,42 +9,27 @@ public class ConexionMySQL {
     private Connection conn;
 
     public Connection open() {
-        // Datos de autenticación (Variables de tu Railway)
-        String user = "root";
-        String password = "GIzvEZkDlYwpINLdXtxwzfmPdWWfRcDw";
-        
-        /**
-         * CONFIGURACIÓN INTERNA PARA RAILWAY:
-         * Host: mysql.railway.internal (MYSQLHOST)
-         * Puerto: 3306 (MYSQLPORT interno)
-         * BD: railway (MYSQLDATABASE)
-         */
-        String url = "jdbc:mysql://mysql.railway.internal:3306/railway"
-                   + "?useUnicode=true"
-                   + "&characterEncoding=utf-8"
-                   + "&useSSL=false"
-                   + "&allowPublicKeyRetrieval=true"
-                   + "&serverTimezone=UTC";
+    String user = "root";
+    String password = "GIzvEZkDlYwpINLdXtxwzfmPdWWfRcDw";
+    
+    // USA ESTA URL EXACTA (Red interna de Railway)
+    // Cambiamos el host a 'mysql.railway.internal' y el puerto al estándar '3306'
+    String url = "jdbc:mysql://mysql.railway.internal:3306/railway"
+               + "?useUnicode=true"
+               + "&characterEncoding=utf-8"
+               + "&useSSL=false"
+               + "&allowPublicKeyRetrieval=true"
+               + "&serverTimezone=UTC";
 
-        try {
-            // Cargamos el driver de MySQL 8
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            
-            // Intentamos la conexión a la red privada de Railway
-            conn = DriverManager.getConnection(url, user, password);
-            System.out.println(">>> CONEXIÓN EXITOSA A LA RED INTERNA DE RAILWAY <<<");
-            return conn;
-            
-        } catch (ClassNotFoundException e) {
-            System.err.println("ERROR: No se encontró el Driver de MySQL.");
-            e.printStackTrace();
-            throw new RuntimeException(e);
-        } catch (SQLException e) {
-            System.err.println("ERROR: Falló la conexión interna a la base de datos.");
-            e.printStackTrace();
-            throw new RuntimeException(e);
-        }
+    try {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        conn = DriverManager.getConnection(url, user, password);
+        return conn;
+    } catch (Exception e) {
+        e.printStackTrace();
+        throw new RuntimeException("Error al conectar con la BD interna: " + e.getMessage());
     }
+}
 
     public void close() {
         if (conn != null) {
