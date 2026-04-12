bfed17a3c95c9befaaee3bd92e49bad28c3719cc
JetMacrogamer <marcosalbzer@hotmail.com>
2026-04-12 15:29:01 -0600

    correo

--- IntegradoraWeb_core/src/org/utl/dsm/integradoraweb/controller/ControllerUsuarios.java
+++ IntegradoraWeb_core/src/org/utl/dsm/integradoraweb/controller/ControllerUsuarios.java
@@ -166,9 +166,10 @@ public class ControllerUsuarios {
 
         Properties props = new Properties();
         props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.smtp.host", "smtp.gmail.com");
         props.put("mail.smtp.port", "465");
+        props.put("mail.smtp.socketFactory.port", "465"); // Requerido para SSL en algunos servidores
+        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory"); // Forzar SSL
         props.put("mail.smtp.ssl.enable", "true");
 
         Session session = Session.getInstance(props, new jakarta.mail.Authenticator() {
